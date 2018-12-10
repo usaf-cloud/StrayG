63c7d0c3896dd55736a4fdcdf04b1185fbc9034b
Vincent Giacomino <vg@quadfeather.com>
2018-12-09 21:31:39 -0500

    minors

---
+++ StrayG.MDM/IMDMClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StrayG.MDM
+{
+    public interface IMDMClient
+    {
+        #region Getting Pertinent Information
+        /// <summary>
+        /// Gets the Uri that the MDMClient is working with.
+        /// </summary>
+        /// <returns>Service uri.</returns>
+        Uri GetServiceUri();
+        #endregion Getting Pertinent Information
+
+    }
+}
---
+++ StrayG.Test/MDMTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Data;
+using System.Net;
+using System.IO;
+using System.ServiceModel;
+using StrayG.MDM;
+using System.Diagnostics;
+using StrayG.Core.Extensions;
+
+namespace StrayG.Test
+{
+    [TestClass]
+    public class MDMTest
+    {
+        [TestMethod]
+        public async Task ItemSearch()
+        {
+        }
+    }
+}
